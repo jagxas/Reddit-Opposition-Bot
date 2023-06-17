3629c5085a006da90c15bcb9d73ad62808138931
jagxas <erdem1umut1iel@gmail.com>
2023-06-17 11:18:56 +0200

    -

---
+++ main.py
@@ -0,0 +1,89 @@
+import praw
+import os
+import constants
+import random
+import re
+from unidecode import unidecode
+from datetime import datetime
+from keep_alive import keep_alive
+
+reddit = praw.Reddit(client_id=os.getenv('client_id'),
+                     client_secret=os.getenv('client_secret'),
+                     username=os.getenv('username'),
+                     password=os.getenv('password'),
+                     user_agent="<OppositionBot1.0>")
+
+
+def preProcess(comment):
+  comment = comment.lower()
+  comment = re.sub(r'[^A-Za-z0-9 ]+', '', comment)
+  comment = unidecode(comment)
+
+  return comment
+
+
+lastPosted = 0
+
+
+class OppositionBot:
+
+  def __init__(self):
+    global lastPosted
+    self.responses = constants.clauses
+    #lastPosted = datetime.now().minute
+    lastPosted = 0
+
+  def find_fitting_comment(self, comment):
+    print(abs(lastPosted - datetime.now().minute))
+    for i in constants.keywords:
+      if preProcess(comment.body).__contains__(i):
+        if self.avoid_ban():
+          #print(abs(lastPosted - datetime.now().minute))
+          print(comment.body)
+          self.send_reply(comment)
+          break
+
+  def avoid_ban(self):
+    global lastPosted
+    if (abs(lastPosted - datetime.now().minute)) >= 10:
+      return True
+    elif (abs(lastPosted - datetime.now().minute)) < 10:
+      return False
+
+  def send_reply(self, comment):
+    global lastPosted
+    replyMessage = self.takeRandomThree()
+    try:
+      comment.reply(replyMessage)
+      #print("ICERDEYIZ")
+      print(replyMessage)
+      lastPosted = datetime.now().minute
+    except Exception as e:
+      pass
+      #print("DISARDAYIZ")
+
+  def takeRandomThree(self):
+    first = constants.clauses[random.randint(0, len(constants.clauses))]
+    second = constants.clauses[random.randint(0, len(constants.clauses))]
+    third = constants.clauses[random.randint(0, len(constants.clauses))]
+
+    reply_text = """
+    Merhaba, yorumunda siyasi ögeler bulunduğu için bu mesajı yazıyorum. Aşağıda 6'lı Masanın Mutabakat Metni'nden rastgele 3 madde bırakıyorum:
+    
+    1){}
+    2){}
+    3){}
+
+    Birçok insan uzun yazılar okumayı sevmiyor. Eh, haklılar. Benim, yani Altili-Masa-Bot'un amacı insanlara bu maddeleri olabildiğince duyurabilmek. Herhangi bir siyasi partiyle alakam yok ve tamamen bağımsız 3. kişiler tarafından programlandım. İyi günler dilerim :)
+    """.format(first, second, third)
+    #print(reply_text)
+    return reply_text
+
+
+keep_alive()
+
+bot = OppositionBot()
+subreddit = reddit.subreddit("turkey")
+
+for comment in subreddit.stream.comments(skip_existing=True):
+  bot.find_fitting_comment(comment)
